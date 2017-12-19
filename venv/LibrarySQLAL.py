9913af85bcd895460d84f8783b019c519ce7eb05
Jeremy Stookey <mrjeremystookey@gmail.com>
2017-12-19 16:01:48 -0700

    full string search is working, substring search still not working

---
+++ venv/LibrarySQLAL.py
@@ -0,0 +1,91 @@
+from flask import Flask, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:kennedy@localhost:3306/library"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class library(db.Model):
+    __table__name = 'Library'
+    id = db.Column('bookID',db.Integer, primary_key=True)
+    title = db.Column('title',db.String(45), nullable=False)
+    author = db.Column('author',db.String(45), nullable=False)
+    date_obtained = db.Column('dateObtained',db.String(45),nullable=True)
+    subtitle = db.Column('subtitle',db.String(45),nullable=True)
+    finished = db.Column('finished',db.Integer)
+    genre = db.Column('genre', db.String(45))
+
+    # def __repr__(self):
+    #     return "Library": {'title': self.title,
+    #                         'subtitle': self.subtitle,
+    #                         'author': self.author,
+    #                         'genre': self.genre,
+    #                         'dateObtained': self.date_obtained,
+    #                         'finished:': self.finished}}
+
+    def __init__(self, id, title, author, subtitle, genre, date_obtained, finished):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.subtitle = subtitle
+        self.genre = genre
+        self.date_obtained = date_obtained
+        self.finished = finished
+
+    def _asdict(self):
+        result = OrderedDict()
+        for key in self.__mapper__.c.keys():
+            result[key] = getattr(self, key)
+        return result
+
+    def serialize(self):
+        return{
+            'id' : self.id,
+            'title' : self.title,
+            'subtitle' : self.subtitle,
+            'author' : self.author,
+            'genre' : self.genre,
+            'date Obtained' : self.date_obtained,
+            'finished' : self.finished
+        }
+
+
+@app.route('/')
+def index():
+    return "Index Page - Welcome to Jeremy's Webservice"
+
+
+#Get all books (entries)
+@app.route('/library/api/v1.0/books/', methods = ['GET'])
+def get_all_books():
+    list_of_books = []
+    for books in library.query.all():
+        list_of_books.append(library.serialize(books))
+    return jsonify({'library': list_of_books})
+
+#Get book whose 'author' contains author
+@app.route('/library/api/v1.0/books/<string:chosen_author>', methods=['GET'])
+def get_author_books(chosen_author):
+    ouevre = []
+    authorQuery = library.author.ilike(chosen_author)
+    #Debug
+    print(authorQuery)
+    #Debug
+    print(type(authorQuery))
+    #Works with full string queries, not substring queries
+    query = library.query.filter(authorQuery).all()
+    print(type(query))
+    print(len(query))
+    for title in query:
+        ouevre.append(library.serialize(title))
+        print(type(title))
+    print(type(ouevre))
+    return jsonify({'bibliography': ouevre})
+
+
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
